c57a7979706f0e5f02a525b469682e69e3439148
robert-trirop <schuetze.r@web.de>
2021-05-06 17:35:27 +0200

    Edited verlet.cpp and verlet.h

--- verlet.cpp
+++ verlet.cpp
@@ -1,4 +1,11 @@
-//
-// Created by schue on 06.05.2021.
-//
+#include "verlet.h"
 
+void verlet_step1(double &x, double &y, double &z, double &vx, double &vy, double &vz,
+                  double fx, double fy, double fz, double timestep) {
+    ... implement Verlet step1 here ...
+}
+
+void verlet_step2(double &vx, double &vy, double &vz, double fx, double fy, double fz,
+                  double timestep) {
+    ... implement Verlet step2 here ...
+}
--- verlet.h
+++ verlet.h
@@ -1,8 +1,9 @@
-//
-// Created by schue on 06.05.2021.
-//
+#ifndef __VERLET_H
+#define __VERLET_H
 
-#ifndef MYPROJECT_VERLET_H
-#define MYPROJECT_VERLET_H
+void verlet_step1(double &x, double &y, double &z, double &vx, double &vy, double &vz,
+                  double fx, double fy, double fz, double timestep);
+void verlet_step2(double &vx, double &vy, double &vz, double fx, double fy, double fz,
+                  double timestep);
 
-#endif //MYPROJECT_VERLET_H
+#endif  // __VERLET_H
