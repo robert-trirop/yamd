39217105f8d258fc1b3500365494bef96486786e
robert-trirop <schuetze.r@web.de>
2021-09-29 15:15:15 +0200

    final

--- tests/test_verlet.cpp
+++ tests/test_verlet.cpp
@@ -24,6 +24,7 @@
 
 #include <gtest/gtest.h>
 #include "HeaderFiles/verlet.h"
+#include "HeaderFiles/atoms.h"
 
 // test atom movement for simple model problem: constant force and random initial conditions
 TEST(VerletTest, BasicAssertions) {
@@ -36,13 +37,37 @@ TEST(VerletTest, BasicAssertions) {
     double timestep = 0.1;
     int nb_steps = 10000; // 10k time steps
     for (int i = 0; i < nb_steps; ++i) {
-        verlet_step1_simple(p, v, f, timestep); // not force update needed
+        verlet_step1_simple(p, v, f, timestep); // no force update needed
         verlet_step2_simple(v, f, timestep);
     }
     double t = timestep*nb_steps; // total time
     for(int dim = 0; dim<3; dim++){ // check if parabola in all three dimensions
         for(int atom = 0; atom<n_atoms; atom++){
             EXPECT_NEAR(p(dim,atom), p0(dim,atom)+v0(dim,atom)*t+0.5*f(dim,atom)*t*t, 1e-6);
+        }
+    }
+}
+
+// ATOM CLASS edition: test atom movement for simple model problem: constant force and random initial conditions
+TEST(VerletTest, BasicAssertionsAtomClass) {
+    int n_atoms = 100; // 100 atoms
+    Positions_t p = Positions_t::Random(3,n_atoms); // atom positions
+    Positions_t p0 = p;
+    Velocities_t v = Velocities_t::Random(3,n_atoms); // atom velocities
+    Velocities_t v0 = v;
+    Forces_t f = Forces_t::Random(3,n_atoms); // atom forces
+    Atoms atoms(p0,v0); // use atom class to test corresponding verlet integrator
+    atoms.forces = f;
+    double timestep = 0.1;
+    int nb_steps = 10000; // 10k time steps
+    for (int i = 0; i < nb_steps; ++i) {
+        verlet_step1(atoms, timestep); // no force update needed
+        verlet_step2(atoms, timestep);
+    }
+    double t = timestep*nb_steps; // total time
+    for(int dim = 0; dim<3; dim++){ // check if parabola in all three dimensions
+        for(int atom = 0; atom<n_atoms; atom++){
+            EXPECT_NEAR(atoms.positions(dim,atom), p0(dim,atom)+v0(dim,atom)*t+0.5*f(dim,atom)*t*t, 1e-6);
         }
     }
 }
